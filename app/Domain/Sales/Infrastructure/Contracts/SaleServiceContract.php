c495490d4ac8177c6b69b0689bfcf497b615bfa9
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2024-03-07 18:43:27 -0300

    refactor(sale): add new method to sale service contract

--- app/Domain/Sales/Infrastructure/Contracts/SaleServiceContract.php
+++ app/Domain/Sales/Infrastructure/Contracts/SaleServiceContract.php
@@ -10,5 +10,6 @@ interface SaleServiceContract
     public function getSales(): Collection;
     public function getSale(string $id): Sale;
     public function newSale(array $payload): Sale;
+    public function addProduct(array $payload, string $id): Sale;
     public function cancel(string $id): Sale;
 }
